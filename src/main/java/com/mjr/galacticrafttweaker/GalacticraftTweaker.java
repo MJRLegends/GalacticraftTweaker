187f2b809944d653dfbb16d48d011ab75ab55286
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-21 22:19:55 +0000

    Released v1.0.0

--- src/main/java/com/mjr/galacticrafttweaker/GalacticraftTweaker.java
+++ src/main/java/com/mjr/galacticrafttweaker/GalacticraftTweaker.java
@@ -4,11 +4,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = GalacticraftTweaker.MODID, version = GalacticraftTweaker.VERSION, dependencies = "required-after:galacticraftcore; required-after:galacticraftplanets; required-after:crafttweaker@[3.0.25.,")
+@Mod(modid = GalacticraftTweaker.MODID, version = GalacticraftTweaker.VERSION, dependencies = "required-after:galacticraftcore; required-after:galacticraftplanets; required-after:crafttweaker@[3.0.25.,); after:extraplanets;" )
 public class GalacticraftTweaker
 {
     public static final String MODID = "galacticrafttweaker";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.12.2-1.0.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
