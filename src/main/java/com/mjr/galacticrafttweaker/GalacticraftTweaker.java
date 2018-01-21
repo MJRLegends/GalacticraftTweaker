605cc2cc6850c4d6d343a9d76a6c16ba96e414cc
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-21 17:30:54 +0000

    Initial commit

---
+++ src/main/java/com/mjr/galacticrafttweaker/GalacticraftTweaker.java
@@ -0,0 +1,17 @@
+package com.mjr.galacticrafttweaker;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = GalacticraftTweaker.MODID, version = GalacticraftTweaker.VERSION)
+public class GalacticraftTweaker
+{
+    public static final String MODID = "galacticrafttweaker";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
