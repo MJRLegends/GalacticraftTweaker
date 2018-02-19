2490951f6e54fc43bd117ea11be6c1b91cacd951
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-19 08:49:34 +0000

    Released v1.0.1 & Added Fingerprint Violation Jar Signing

--- src/main/java/com/mjr/galacticrafttweaker/GalacticraftTweaker.java
+++ src/main/java/com/mjr/galacticrafttweaker/GalacticraftTweaker.java
@@ -2,16 +2,26 @@ package com.mjr.galacticrafttweaker;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = GalacticraftTweaker.MODID, version = GalacticraftTweaker.VERSION, dependencies = "required-after:galacticraftcore; required-after:galacticraftplanets; required-after:crafttweaker@[3.0.25.,); after:extraplanets;" )
-public class GalacticraftTweaker
-{
-    public static final String MODID = "galacticrafttweaker";
-    public static final String VERSION = "1.12.2-1.0.0";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-    }
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.mjr.extraplanets.Constants;
+
+@Mod(modid = GalacticraftTweaker.MODID, version = GalacticraftTweaker.VERSION, dependencies = "required-after:galacticraftcore@[4.0.1.138,); required-after:galacticraftplanets@[4.0.1.138,); required-after:crafttweaker@[3.0.25.,); after:extraplanets;", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
+public class GalacticraftTweaker {
+	public static final String MODID = "galacticrafttweaker";
+	public static final String VERSION = "1.12.2-1.0.1";
+	public static final Logger logger = LogManager.getLogger("galacticrafttweaker");
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@EventHandler
+	public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
+		logger.error(Constants.modID + " Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported!");
+	}
 }
