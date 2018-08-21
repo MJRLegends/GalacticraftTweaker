052e098db768d7b6358d95ba6e0c383a1a43dee2
MJRLegends <MJRLegends@users.noreply.github.com>
2018-08-21 17:22:20 +0100

    Released v1.0.2 Set required GC version requirement to Build 181

--- src/main/java/com/mjr/galacticrafttweaker/GalacticraftTweaker.java
+++ src/main/java/com/mjr/galacticrafttweaker/GalacticraftTweaker.java
@@ -8,10 +8,10 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = GalacticraftTweaker.MODID, version = GalacticraftTweaker.VERSION, dependencies = "required-after:galacticraftcore@[4.0.1.138,); required-after:galacticraftplanets@[4.0.1.138,); required-after:crafttweaker@[3.0.25.,); after:extraplanets;", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
+@Mod(modid = GalacticraftTweaker.MODID, version = GalacticraftTweaker.VERSION, dependencies = "required-after:galacticraftcore@[4.0.1.181,); required-after:galacticraftplanets@[4.0.1.181,); required-after:crafttweaker@[3.0.25.,); after:extraplanets;", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
 public class GalacticraftTweaker {
 	public static final String MODID = "galacticrafttweaker";
-	public static final String VERSION = "1.12.2-1.0.1";
+	public static final String VERSION = "1.12.2-1.0.2";
 	public static final Logger logger = LogManager.getLogger("galacticrafttweaker");
 
 	@EventHandler
