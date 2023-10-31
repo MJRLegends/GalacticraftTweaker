e492ea87846bc9aecde2aa3886caa90dd5553da2
MJRLegends <MJRLegends@users.noreply.github.com>
2023-10-31 23:24:47 +0000

    Added a name to the @Mod annotation so the mod shows with a better name in client mods list

--- src/main/java/com/mjr/galacticrafttweaker/GalacticraftTweaker.java
+++ src/main/java/com/mjr/galacticrafttweaker/GalacticraftTweaker.java
@@ -8,9 +8,10 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = GalacticraftTweaker.MODID, version = GalacticraftTweaker.VERSION, dependencies = "required-after:galacticraftcore@[4.0.1.181,); required-after:galacticraftplanets@[4.0.1.181,); required-after:crafttweaker@[3.0.25.,); after:extraplanets;", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
+@Mod(modid = GalacticraftTweaker.MODID, name = GalacticraftTweaker.NAME, version = GalacticraftTweaker.VERSION, dependencies = "required-after:galacticraftcore@[4.0.1.181,); required-after:galacticraftplanets@[4.0.1.181,); required-after:crafttweaker@[3.0.25.,); after:extraplanets;", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
 public class GalacticraftTweaker {
 	public static final String MODID = "galacticrafttweaker";
+	public static final String NAME = "Galacticraft Tweaker";
 	public static final String VERSION = "1.12.2-1.0.3";
 	public static final Logger logger = LogManager.getLogger("galacticrafttweaker");
 
